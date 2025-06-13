37f500315a57da684fa8947305291c6b3f50437c
Timofey <timworkey@mail.ru>
2025-06-13 19:56:35 +0300

    fix manifest

---
+++ app/src/main/java/com/example/lesson_1/model/FeedModel.kt
@@ -0,0 +1,11 @@
+package com.example.lesson_1.model
+
+import com.example.lesson_1.dto.Post
+
+data class FeedModel(
+    val posts: List<Post> = emptyList(),
+    val loading: Boolean = false,
+    val error: Boolean = false,
+    val empty: Boolean = false,
+    val refreshing: Boolean = false,
+)
