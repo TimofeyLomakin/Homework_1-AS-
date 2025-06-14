30991e5559252a39553a6873338c95465ae1dfd5
Timofey <timworkey@mail.ru>
2025-06-14 14:10:44 +0300

    fix homework-1

--- app/src/main/java/com/example/lesson_1/dto/Post.kt
+++ app/src/main/java/com/example/lesson_1/dto/Post.kt
@@ -1,12 +1,16 @@
 package com.example.lesson_1.dto
 
+import com.google.gson.annotations.SerializedName
+
 
 data class Post(
     val id: Long,
     val author: String,
     val published: String,
     val content: String,
+    @SerializedName("likedByMe")
     val likeByMe: Boolean,
+    @SerializedName("likes")
     val likesCounter: Long,
     val shareCounter: Long,
     val viewsCounter: Long,
